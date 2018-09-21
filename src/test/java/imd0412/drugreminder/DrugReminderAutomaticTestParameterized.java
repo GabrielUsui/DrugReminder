69b945e638c09a68f7c6bc04dad33b895e30bf7f
Gabriel Araujo <marcos.gabriel.araujo1996@gmail.com>
2018-09-21 16:40:17 -0300

    Corrigindo erros dos testes

--- src/test/java/imd0412/drugreminder/DrugReminderAutomaticTestParameterized.java
+++ src/test/java/imd0412/drugreminder/DrugReminderAutomaticTestParameterized.java
@@ -1,4 +1,9 @@
+package imd0412.drugreminder;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -13,17 +18,17 @@ public class DrugReminderAutomaticTestParameterized
 	{
 		return Arrays.asList(new Object[][]
 		{
-			{"10/12/16 14:30", "6 horas", 1, Arrays.asList({"10/12/16 14:30", "10/12/16 20:30", "11/12/16 02:30", "11/12/16 08:30", "11/12/16 14:30"})},
-			{"10/12/16 14:30", "8 horas", 1, Arrays.asList({"10/12/16 14:30", "10/12/16 22:30", "11/12/16 06:30"})},
-			{"10/12/16 14:30", "12 horas", 1, Arrays.asList({"10/12/16 14:30", "11/12/16 02:30"})},
-			{"10/12/16 14:30", "24 horas", 1, Arrays.asList({"10/12/16 14:30"})},
+			{"10/12/16 14:30", "6 horas", 1, Arrays.asList("10/12/16 14:30", "10/12/16 20:30", "11/12/16 02:30", "11/12/16 08:30", "11/12/16 14:30")},
+			{"10/12/16 14:30", "8 horas", 1, Arrays.asList("10/12/16 14:30", "10/12/16 22:30", "11/12/16 06:30")},
+			{"10/12/16 14:30", "12 horas", 1, Arrays.asList("10/12/16 14:30", "11/12/16 02:30")},
+			{"10/12/16 14:30", "24 horas", 1, Arrays.asList("10/12/16 14:30")},
 		});
 	}
 	
 	@Parameter(0)
 	public String startTime;
 	@Parameter(1)
-	public String frequency;
+	public Frequency frequency;
 	@Parameter(2)
 	public int duration;
 	@Parameter(3)
@@ -31,10 +36,15 @@ public class DrugReminderAutomaticTestParameterized
 	
 
 	@Test
-	public void testDefineType() {
-	DrugReminderImpl reminder = new DrugReminderImpl();
-	String returnedType = reminder.createReminders(startTime, frequency, duration);
-	assertEquals(expectedType, returnedType);
+	public void testDefineType() 
+	{
+		DrugReminderImpl reminder = new DrugReminderImpl();
+		List<String> returnedType = reminder.createReminders(startTime, frequency, duration);
+	
+		for(int i = 0; i < returnedType.size();i++)
+		{
+			assertEquals(expectedType.get(i), returnedType.get(i));
+		}
 	}
 
 }
